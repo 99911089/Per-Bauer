64d101cdfff7b26ad22bc04daa8d73a7648db099
Per <peba63298@edu.ucl.dk>
2025-03-07 23:15:17 +0100

    opgave 17

---
+++ Opgave 17/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+        public class Addition
+        {
+
+            private int a = 2;
+            private int b = 3;
+            public int Add()
+            {
+
+                return a + b;
+
+            }
+
+            static void Main()
+            {
+                Addition obj = new Addition();
+                int sum = obj.Add();
+                Console.WriteLine($"Summen er: {sum}");
+                Console.ReadLine();
+            }
+
+        }
+
+
+
